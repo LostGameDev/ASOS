77226aa46911fbb1a24e60201c97aeed505fe9db
GameWorld <ehrenf@outlook.com>
2024-02-21 23:54:47 +1000

    everything

---
+++ Commands.py
@@ -0,0 +1,223 @@
+import os
+import subprocess
+import sys
+import shutil
+import Utils
+
+def CommandHelp(login):
+    Utils.OSPrint("Available commands:")
+    for command in commands:
+        Utils.OSPrint(command)
+
+def CheckIfDirectoryExists(directory):
+    char = {"/":'\\', ":":"", '"':''}
+    ConvertedDir = ".\\" + ''.join(char.get(s, s) for s in directory)
+    if os.path.exists(ConvertedDir):
+        return True
+    return False
+
+def CommandDir(arg, login):
+    if os.stat(".\\UserDirectory.txt").st_size == 0:
+        UserDirectory = f"A:/users/{login}/personal_files/"
+        file = open(".\\UserDirectory.txt", "w")
+        file.write(UserDirectory)
+        file.close()
+        char = {"/":'\\', ":":"", '"':''}
+        ConvertedDir = ".\\" + ''.join(char.get(s, s) for s in UserDirectory)
+        os.system(f"cd {ConvertedDir}")
+    else:
+        file = open(".\\UserDirectory.txt", "r")
+        UserDirectory = file.read()
+        file.close()
+        char = {"/":'\\', ":":"", '"':''}
+        ConvertedDir = ".\\" + ''.join(char.get(s, s) for s in UserDirectory)
+        os.system(f"cd {ConvertedDir}")
+        
+    if arg == "getname":
+        Utils.OSPrint(f"Current directory is: {UserDirectory}")
+        char = {"/":'\\', ":":"", '"':''}
+        ConvertedDir = ".\\" + ''.join(char.get(s, s) for s in UserDirectory)
+        FileCount = 0
+        FolderCount = 0
+        for item in os.listdir(ConvertedDir):
+            if os.path.isfile(os.path.join(ConvertedDir, item)):
+                FileCount += 1
+            if os.path.isdir(os.path.join(ConvertedDir, item)):
+                FolderCount += 1
+        if FileCount == 0 and FolderCount == 0:
+            Utils.OSPrint(f"0 files detected. 0 sub-directories detected. Folder is empty.")
+        else:
+            Utils.OSPrint(f"{FileCount} files detected. {FolderCount} sub-directories detected.")
+        return
+    if arg == "getname -P":
+        Utils.OSPrint(f"Current directory is: {UserDirectory}")
+        char = {"/":'\\', ":":"", '"':''}
+        ConvertedDir = ".\\" + ''.join(char.get(s, s) for s in UserDirectory)
+        FileCount = 0
+        FolderCount = 0
+        for item in os.listdir(ConvertedDir):
+            if os.path.isfile(os.path.join(ConvertedDir, item)):
+                FileCount += 1
+                Utils.OSPrint(f"File: {item}")
+            if os.path.isdir(os.path.join(ConvertedDir, item)):
+                FolderCount += 1
+                Utils.OSPrint(f"Sub-directory: {item}")
+        if FileCount == 0 and FolderCount == 0:
+            Utils.OSPrint(f"0 files detected. 0 sub-directories detected. Folder is empty.")
+        else:
+            Utils.OSPrint(f"{FileCount} files detected. {FolderCount} sub-directories detected.")
+        return
+    Exists = CheckIfDirectoryExists(arg)
+    if Exists == False:
+        Utils.OSPrint(f"Directory does not exist!")
+        return
+    file = open(".\\UserDirectory.txt", "w")
+    file.write(arg)
+    file.close()
+    UserDirectory = arg
+    char = {"/":'\\', ":":"", '"':''}
+    ConvertedDir = ".\\" + ''.join(char.get(s, s) for s in UserDirectory)
+    os.system(f"cd {ConvertedDir}")
+    Utils.OSPrint(f"Now located in {arg}")
+
+def CommandExec(program, login):
+    if os.stat(".\\UserDirectory.txt").st_size == 0:
+        UserDirectory = f"A:/users/{login}/personal_files/"
+        file = open(".\\UserDirectory.txt", "w")
+        file.write(UserDirectory)
+        file.close()
+    else:
+        file = open(".\\UserDirectory.txt", "r")
+        UserDirectory = file.read()
+        file.close()
+
+    python_executable = sys.executable  # Path to the Python executable running this script
+    python_path = os.environ.get("PYTHONPATH", "")  # Get the current PYTHONPATH
+    env = os.environ.copy()  # Create a copy of the current environment variables
+    env["PYTHONPATH"] = python_path  # Set the PYTHONPATH for the subprocess
+    char = {"/":'\\', ":":"", '"':''}
+    ConvertedDir = "" + ''.join(char.get(s, s) for s in UserDirectory)
+    for item in os.listdir(ConvertedDir):
+        if os.path.isfile(os.path.join(ConvertedDir, item)) and item == program:
+            if item.endswith(".py"):
+                try:
+                    Utils.OSLoad(f"Booting \"{program}\"", f"\"{program}\" running.", "Normal")
+                    subprocess.run([python_executable, item], env=env, shell=True, cwd = ConvertedDir)
+                except FileNotFoundError:
+                    Utils.OSPrint("File not found.")
+            else:
+                Utils.OSPrint("Only python programs can be executed.")
+            break
+    else:
+        Utils.OSPrint("Program not found.")
+
+def CommandOpen(File, login):
+    if os.stat(".\\UserDirectory.txt").st_size == 0:
+        UserDirectory = f"A:/users/{login}/personal_files/"
+        file = open(".\\UserDirectory.txt", "w")
+        file.write(UserDirectory)
+        file.close()
+    else:
+        file = open(".\\UserDirectory.txt", "r")
+        UserDirectory = file.read()
+        file.close()
+    python_executable = sys.executable  # Path to the Python executable running this script
+    python_path = os.environ.get("PYTHONPATH", "")  # Get the current PYTHONPATH
+    env = os.environ.copy()  # Create a copy of the current environment variables
+    env["PYTHONPATH"] = python_path  # Set the PYTHONPATH for the subprocess
+    char = {"/":'\\', ":":"", '"':''}
+    ConvertedDir = "" + ''.join(char.get(s, s) for s in UserDirectory)
+    Utils.OSProgramLoad(f"Booting \"TextEditor.py\"", f"Aperture Science Text Editor running. Accessing file \"{File}\"", "Normal")
+    subprocess.run([python_executable, "TextEditor.py", File], env=env, shell=True, cwd = "./ROM/")
+
+def CommandList(File, login):
+    if os.stat(".\\UserDirectory.txt").st_size == 0:
+        UserDirectory = f"A:/users/{login}/personal_files/"
+        file = open(".\\UserDirectory.txt", "w")
+        file.write(UserDirectory)
+        file.close()
+    else:
+        file = open(".\\UserDirectory.txt", "r")
+        UserDirectory = file.read()
+        file.close() 
+    char = {"/":'\\', ":":"", '"':''}
+    ConvertedDir = "" + ''.join(char.get(s, s) for s in UserDirectory)
+    f = open(ConvertedDir + File, 'r')
+    data = f.read()
+    for line in data:
+        Utils.OSPrint(line)
+    f.close()
+
+def CommandCreate(Name, Type, login):
+    if os.stat(".\\UserDirectory.txt").st_size == 0:
+        UserDirectory = f"A:/users/{login}/personal_files/"
+        file = open(".\\UserDirectory.txt", "w")
+        file.write(UserDirectory)
+        file.close()
+    else:
+        file = open(".\\UserDirectory.txt", "r")
+        UserDirectory = file.read()
+        file.close() 
+    char = {"/":'\\', ":":"", '"':''}
+    ConvertedDir = "" + ''.join(char.get(s, s) for s in UserDirectory)
+    if Type == "-File":
+        try:
+            Utils.OSLoad(f"Creating file \"{Name}\"...", f"File \"{Name}\" created.", "Normal")
+            open(ConvertedDir + Name + ".txt", "w")
+        except:
+            Utils.OSPrint(f"Failed to create file \"{Name}\"")
+        return
+    elif Type == "-Folder":
+        try:
+            Utils.OSLoad(f"Creating Folder \"{Name}\"...", f"Folder \"{Name}\" created.", "Normal")
+            os.mkdir(ConvertedDir + Name)
+        except:
+            Utils.OSPrint(f"Failed to create folder \"{Name}\"")
+        return
+    else:
+        Utils.OSPrint(f"Invalid Type \"{Type}\"...")
+        return
+
+def CommandDelete(Name, Type, login):
+    if os.stat(".\\UserDirectory.txt").st_size == 0:
+        UserDirectory = f"A:/users/{login}/personal_files/"
+        file = open(".\\UserDirectory.txt", "w")
+        file.write(UserDirectory)
+        file.close()
+    else:
+        file = open(".\\UserDirectory.txt", "r")
+        UserDirectory = file.read()
+        file.close() 
+    char = {"/":'\\', ":":"", '"':''}
+    ConvertedDir = "" + ''.join(char.get(s, s) for s in UserDirectory)
+    if Type == "-File":
+        try:
+            Utils.OSLoad(f"Deleting file \"{Name}\"...", f"File \"{Name}\" deleted.", "Normal")
+            os.remove(ConvertedDir + Name + ".txt")
+        except:
+            Utils.OSPrint(f"File \"{Name}\" does not exist!")
+        return
+    elif Type == "-Folder":
+        try:
+            Utils.OSLoad(f"Deleting Folder \"{Name}\"...", f"Folder \"{Name}\" deleted.", "Normal")
+            shutil.rmtree(ConvertedDir + Name)
+        except:
+            Utils.OSPrint(f"Folder \"{Name}\" does not exist!")
+        return
+    else:
+        Utils.OSPrint(f"Invalid Type \"{Type}\"...")
+        return
+    
+def CommandClear(login):
+    os.system("cls")
+
+commands = {
+    "help": CommandHelp,
+    "dir": CommandDir,
+    "exec": CommandExec,
+    "open": CommandOpen,
+    "list": CommandList,
+    "create": CommandCreate,
+    "delete": CommandDelete,
+    "clear": CommandClear,
+}
---
+++ main.py
@@ -0,0 +1,97 @@
+import os
+import time
+import Utils
+import json
+import Commands
+from Commands import commands
+
+AccreditationLevel = 0
+
+def LoadOS():
+    os.system("cls")
+    Utils.OSPrint("Aperture Science OS ver 0.1A (EXPERIMENTAL!) starting.")
+    Utils.OSLoad("Booting sequence initializing...", "Booting sequence initialized.", "Slow")
+    Utils.OSLoad("Booting sequence completing...", "Booting sequence completed.", "Slow")
+    time.sleep(1)
+    Utils.OSPrint("Running on kernel ApertureScienceKernel ver 1. Processing speed of 4294967296 bytes/ms.")
+    time.sleep(1)
+    OSMain(False)
+
+def CheckIfLoginExists(login):
+    if os.path.exists(f"./accounts/{login}.json") == False:
+        return False
+    return True
+
+def CheckIfPasswordExists(login, password):
+    f = open(f"./accounts/{login}.json")
+    data = json.loads(f.read())
+    if data['password'] != password:
+        return False
+    return True
+
+def GetAccreditationLevel(login):
+    f = open(f"./accounts/{login}.json")
+    data = json.loads(f.read())
+    return data["Accreditation"]
+
+def tokenize_command(command):
+    tokens = []
+    current_token = ''
+    in_quotes = False
+
+    for char in command:
+        if char == ' ' and not in_quotes:
+            if current_token:
+                tokens.append(current_token)
+                current_token = ''
+        elif char == '"':
+            in_quotes = not in_quotes
+        else:
+            current_token += char
+
+    if current_token:
+        tokens.append(current_token)
+
+    return tokens
+
+def OSMain(LoginFail):
+    if LoginFail == True:
+        Utils.OSPrint("Please enter your credentials. Enter login below.")
+    else:
+        Utils.OSPrint("Welcome to Aperture Science OS. Please enter your credentials. Enter login below.")
+    login = Utils.OSInput(True)
+    LoginCheck = CheckIfLoginExists(login)
+    if LoginCheck != True:
+        Utils.OSPrint("Login does not exist! Please try again!")
+        OSMain(True)
+    Utils.OSPrint("Login successfully identified. Please enter your password below.")
+    password = Utils.OSInput(True)
+    PasswordCheck = CheckIfPasswordExists(login, password)
+    if PasswordCheck != True:
+        Utils.OSPrint("Password is incorrect! Please try again!")
+        OSMain(True)
+    Utils.OSPrint(f"Password matching. Logging to account \"{login}\"")
+    time.sleep(1)
+    AccreditationLevel = GetAccreditationLevel(login)
+    Utils.OSPrint(f"Logged in. Account: \"{login}\". Level {AccreditationLevel} Accreditation.")
+    time.sleep(0.5)
+    Utils.OSPrint(f"Hello, user \"{login}\". What do you want to do? Enter \"help\" to show commands. Enter \"dir getname\" to show current directory, and its files. Enter \"exec\" to run a program. Enter \"open\" to open a file.")
+    while True:
+        command = Utils.OSInput(True)
+        tokens = tokenize_command(command)
+        command_lower = tokens[0].lower()
+
+        if command_lower in commands:
+            if len(tokens) > 1:
+                try:
+                    commands[command_lower](*tokens[1:], login)  # Pass all arguments except the command itself
+                except:
+                    Utils.OSPrint(f"Error command \"{command_lower}\" has too many arguments!")
+            else:
+                try:
+                    commands[command_lower](login)
+                except:
+                    Utils.OSPrint(f"Error command \"{command_lower}\" has too few arguments!")
+        else:
+            Utils.OSPrint("Invalid command. Enter \"help\" to see available commands.")
+LoadOS()
