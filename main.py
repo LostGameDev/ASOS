6cd32d8f61e54f0e3bc785195f3f1b7d4fd9b1b4
GameWorld <ehrenf@outlook.com>
2024-05-27 00:27:27 +1000

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,21 @@ registry = configparser.ConfigParser()
 AdriveExists = True
 OSVersion = ""
 
+def get_base_path():
+    return os.path.abspath(".")
+
+def get_absolute_path(relative_path):
+    return os.path.join(get_base_path(), relative_path)
+
 def LoadOS():
     Utils.OSClearLatestLog()
     os.system("cls")
-    registry.read('.\OSRegistry.ini')
+    registry.read(get_absolute_path('OSRegistry.ini'))
     registry.set('AOS', 'Quit', "False")
-    with open('.\OSRegistry.ini', "w") as registryfile:
+    with open(get_absolute_path('OSRegistry.ini'), "w") as registryfile:
         registry.write(registryfile)
     registry.set('AOS', 'Reboot', "False")
-    with open('.\OSRegistry.ini', "w") as registryfile:
+    with open(get_absolute_path('OSRegistry.ini'), "w") as registryfile:
         registry.write(registryfile)
     OSVersion = registry.get('AOS', 'version')
     Utils.OSPrint(f"{OSVersion} starting.")
@@ -29,58 +35,57 @@ def LoadOS():
     Utils.OSLoad("Booting sequence completing...", "Booting sequence completed.", "Normal")
     time.sleep(1)
     Utils.OSPrint("Running on kernel ApertureScienceKernel ver 1. Processing speed of 4294967296 bytes/ms.")
-    winsound.PlaySound("./sounds/startup.wav", winsound.SND_FILENAME)
+    winsound.PlaySound(get_absolute_path("sounds/startup.wav"), winsound.SND_FILENAME)
     OSMain(False)
 
 def CheckIfLoginExists(login):
-    if os.path.exists(f"./accounts/{login}.json") == False:
-        winsound.PlaySound("./sounds/error2.wav", winsound.SND_FILENAME)
+    if not os.path.exists(get_absolute_path(f"accounts/{login}.json")):
+        winsound.PlaySound(get_absolute_path("sounds/error2.wav"), winsound.SND_FILENAME)
         return False
-    winsound.PlaySound("./sounds/correct.wav", winsound.SND_FILENAME)
+    winsound.PlaySound(get_absolute_path("sounds/correct.wav"), winsound.SND_FILENAME)
     return True
 
 def SetCurrentUser(login):
-    registry.read('.\OSRegistry.ini')
+    registry.read(get_absolute_path('OSRegistry.ini'))
     registry.set('AOS', 'CurrentUser', login)
-    with open('.\OSRegistry.ini', "w") as registryfile:
+    with open(get_absolute_path('OSRegistry.ini'), "w") as registryfile:
         registry.write(registryfile)
 
 def SetUserDirectory(login):
-    registry.read('.\OSRegistry.ini')
+    registry.read(get_absolute_path('OSRegistry.ini'))
     login = registry.get('AOS', 'CurrentUser')
     if registry.get('AOS', 'UserDirectory') == "":
-        UserDirectory = f"A:/users/{login}/personal_files/"
-        registry.read('.\OSRegistry.ini')
+        UserDirectory = get_absolute_path(f"A/users/{login}/personal_files/")
+        registry.read(get_absolute_path('OSRegistry.ini'))
         registry.set('AOS', 'UserDirectory', UserDirectory)
-        with open('.\OSRegistry.ini', "w") as registryfile:
+        with open(get_absolute_path('OSRegistry.ini'), "w") as registryfile:
             registry.write(registryfile)
 
 def CheckIfPasswordExists(login, password):
-    f = open(f"./accounts/{login}.json")
-    data = json.loads(f.read())
+    with open(get_absolute_path(f"accounts/{login}.json")) as f:
+        data = json.loads(f.read())
     if data['password'] != password:
-        winsound.PlaySound("./sounds/error2.wav", winsound.SND_FILENAME)
+        winsound.PlaySound(get_absolute_path("sounds/error2.wav"), winsound.SND_FILENAME)
         return False
-    winsound.PlaySound("./sounds/correct.wav", winsound.SND_FILENAME)
+    winsound.PlaySound(get_absolute_path("sounds/correct.wav"), winsound.SND_FILENAME)
     return True
 
 def GetAccreditationLevel(login):
-    f = open(f"./accounts/{login}.json")
-    data = json.loads(f.read())
+    with open(get_absolute_path(f"accounts/{login}.json")) as f:
+        data = json.loads(f.read())
     return data["accreditation"]
 
 def CheckIfADriveExists(login):
-    #This function checks if A drive exists, and if other system critical folders exist, if any system critical folders are missing then it will create them
-    if os.path.exists("./A"):
-        if os.path.exists(f"./A/users/{login}/personal_files") != True:
-            if os.path.exists(f"./A/users/") != True:
-                os.mkdir("./A/users/")
-            if os.path.exists(f"./A/users/{login}") != True:
-                os.mkdir(f"./A/users/{login}")
-            os.mkdir(f"./A/users/{login}/personal_files")
-        if os.path.exists("./A/logs/") != True:
-            os.mkdir("./A/logs/")
-            with open("./A/logs" + ".meta", "wb") as file:
+    if os.path.exists(get_absolute_path("A")):
+        if not os.path.exists(get_absolute_path(f"A/users/{login}/personal_files")):
+            if not os.path.exists(get_absolute_path("A/users/")):
+                os.mkdir(get_absolute_path("A/users/"))
+            if not os.path.exists(get_absolute_path(f"A/users/{login}")):
+                os.mkdir(get_absolute_path(f"A/users/{login}"))
+            os.mkdir(get_absolute_path(f"A/users/{login}/personal_files"))
+        if not os.path.exists(get_absolute_path("A/logs/")):
+            os.mkdir(get_absolute_path("A/logs/"))
+            with open(get_absolute_path("A/logs") + ".meta", "wb") as file:
                 binary_data = struct.pack('i', 2)
                 file.write(binary_data)
                 file.close()
@@ -109,15 +114,15 @@ def tokenize_command(command):
 
 def OSMain(LoginFail):
     registry.set('AOS', 'LoggedOut', "False")
-    with open('.\OSRegistry.ini', "w") as registryfile:
+    with open(get_absolute_path('OSRegistry.ini'), "w") as registryfile:
         registry.write(registryfile)
-    if LoginFail == True:
+    if LoginFail:
         Utils.OSPrint("Please enter your credentials. Enter login below.")
     else:
         Utils.OSPrint("Welcome to the Aperture Science OS. Please enter your credentials. Enter login below.")
     login = Utils.OSInput(True)
     LoginCheck = CheckIfLoginExists(login)
-    if LoginCheck != True:
+    if not LoginCheck:
         Utils.OSPrintWarning("Login does not exist! Please try again")
         OSMain(True)
     Utils.OSPrint("Login successfully identified. Please enter your password below.")
@@ -125,21 +130,21 @@ def OSMain(LoginFail):
     SetUserDirectory(login)
     password = Utils.OSInput(True)
     PasswordCheck = CheckIfPasswordExists(login, password)
-    if PasswordCheck != True:
+    if not PasswordCheck:
         Utils.OSPrintWarning("Password is incorrect! Please try again")
         OSMain(True)
     Utils.OSPrint(f"Password matching. Logging to account \"{login}\"")
     time.sleep(1)
     AccreditationLevel = GetAccreditationLevel(login)
     AdriveExists = CheckIfADriveExists(login)
-    if AdriveExists != True:
+    if not AdriveExists:
         Utils.OSPrintError("FATAL ERROR: A drive does not exist")
         sys.exit(1)
     Utils.OSPrint(f"Logged in. Account: \"{login}\". Level {AccreditationLevel} Accreditation.")
     time.sleep(0.5)
     Utils.OSPrint(f"Hello, user \"{login}\". What do you want to do? Enter \"help\" to show commands. Enter \"dir getname\" to show current directory, and its files. Enter \"exec\" to run a program. Enter \"open\" to open a file.")
     while True:
-        registry.read(".\OSRegistry.ini")
+        registry.read(get_absolute_path("OSRegistry.ini"))
         if registry.get('AOS', 'Reboot') == "True":
             LoadOS()
         elif registry.get('AOS', 'loggedout') == "True":
@@ -174,4 +179,5 @@ def OSMain(LoginFail):
 
         else:
             Utils.OSPrintWarning("Invalid command. Enter \"help\" to see available commands.")
+
 LoadOS()
